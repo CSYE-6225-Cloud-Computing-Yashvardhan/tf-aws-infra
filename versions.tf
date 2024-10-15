b1002169a2863dddcde79f7d986bc2094e84265d
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-10-14 20:02:50 -0400

    positive

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-                    aws = {
-        = "hashicorp/aws"
+    aws = {
+      source  = "hashicorp/aws"
       version = ">5.0.0, <6.0.0"
     }
   }
