6523b0428d56de3e8d7aaf486e29d0efd09e3275
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-10-16 02:11:56 -0400

    ec2 updated

--- ec2.tf
+++ ec2.tf
@@ -4,15 +4,14 @@ resource "aws_instance" "web_app" {
   subnet_id                   = aws_subnet.public[0].id
   security_groups             = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
+  key_name                    = "aws-dev"
+  disable_api_termination     = false
 
   root_block_device {
     volume_size           = 25
     volume_type           = "gp2"
     delete_on_termination = true
   }
-
-  disable_api_termination = false
-  key_name                = "aws-dev"
 
   tags = {
     Name = "webapp-${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
