ab0afb2ac6be8355f736153528f13191b7b3bc82
Yashvardhan Limbodiya <limbodiya.y@northeastern.edu>
2024-11-19 01:56:03 -0500

    lambdaSG, NAT EIP changes

--- iam.tf
+++ iam.tf
@@ -78,11 +78,6 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "rds_access" {
-  role       = aws_iam_role.lambda_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
